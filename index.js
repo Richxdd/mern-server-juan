a75c5443a8631a27f83ca906d5eb9757db81cda1
RICARDO SOLIS <ricardosv46@gmail.com>
2022-02-18 22:24:17 -0500

    fix

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const conectarDB = require('./config/db');
-const cors = require('cors');
+const express = require("express")
+const conectarDB = require("./config/db")
+const cors = require("cors")
 
 // crear el servidor
-const app = express();
+const app = express()
 
 // Conectar a la base de datos
-conectarDB();
+conectarDB()
 
 // habilitar cors
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }))
+app.options("*", cors())
 
 // Habilitar express.json
-app.use( express.json({ extended: true }));
+app.use(express.json({ extended: true }))
 
 // puerto de la app
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
 // Importar rutas
-app.use('/api/usuarios', require('./routes/usuarios'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/proyectos', require('./routes/proyectos'));
-app.use('/api/tareas', require('./routes/tareas'));
+app.use("/api/usuarios", require("./routes/usuarios"))
+app.use("/api/auth", require("./routes/auth"))
+app.use("/api/proyectos", require("./routes/proyectos"))
+app.use("/api/tareas", require("./routes/tareas"))
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
-});
+app.listen(port, "0.0.0.0", () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
